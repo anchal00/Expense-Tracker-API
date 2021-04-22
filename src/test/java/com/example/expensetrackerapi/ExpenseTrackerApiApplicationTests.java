eddebbb2295fd06b12473b73a4c435334c42724f
anchal00 <anchal82199@gmail.com>
2021-04-22 13:34:42 +0530

    Initialized the Spring boot project

---
+++ src/test/java/com/example/expensetrackerapi/ExpenseTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expensetrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
